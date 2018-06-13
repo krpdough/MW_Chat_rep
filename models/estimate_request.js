e09f6a180deeec32055b31c49f1d1d15533b876f
krpdough <krpdough@gmail.com>
2018-06-13 12:28:58 -0400

    Lunch time programming - added a file for pulling from mongo, kept seperate in case Mike has something for it. Set up the email, will need to see it before I decide on the styling but all variables are there.

--- models/estimate_request.js
+++ models/estimate_request.js
@@ -2,19 +2,20 @@
 //JSON in a POST body
 
 
-// {
-//   "acres": Decimal,
-//     "services": Comma delimited String,
-//       "contact_email": String,
-//         "contact_phone": String,
-// }
-
 const pug = require('pug');
 
 // Compile the template file 
 const compiledFunction = pug.compileFile('./templates/basic_estimate.pug');
 
-// Render a set of data
+//Get the data from Mongo
+response = mogno_data_pull('collection or whatever we need to pass, if anything');
+
+// Render a set of data - this sends it to test, I believe
 console.log(compiledFunction({
-  name: 'KKKEEEVVIIINNN'
+  acres: response['acres'],
+  services: response['services'],
+  contact_email: response['contact_email'],
+  contact_phone: response['contact_phone'],
 }));
+
+
